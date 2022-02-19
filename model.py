2fd3a4f1d714cb49dae46b8d30392f92540e2746
Ignasi <ignasi@annamarco.com>
2022-02-19 12:36:12 +0100

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   model.py

--- model.py
+++ model.py
@@ -10,13 +10,13 @@ class CBOW(nn.Module):
         self.emb = nn.Embedding(num_embeddings, embedding_dim, padding_idx=0)
         self.lin = nn.Linear(embedding_dim, num_embeddings, bias=False)
 
-        if isinstance(weigths, (torch.Tensor, np.ndarray, list, tuple)): # TODO: cualquier cosa que sirva para meter numeros a pelo
-            self.weigths = torch.Tensor(weigths, requires_grad=False)
+        if isinstance(weigths, (torch.Tensor, np.ndarray, list, tuple)):
+            self.weigths = nn.Parameter(torch.Tensor(weigths), requires_grad=False)
+            assert len(self.weigths.shape) == 1
         elif isinstance(weigths, torch.dtype):
-            self.weigths = # TODO: trainable Tensor of num_context_words
-        else: # Includes it being None
-            self.weigths = torch.ones(num_context_words, requires_grad=False)
-
+            self.weigths = nn.Parameter(torch.rand(num_context_words, dtype=weigths), requires_grad=True)
+        else: # Includes it being None (default)
+            self.weigths = nn.Parameter(torch.ones(num_context_words), requires_grad=False)
 
     # B = Batch size
     # W = Number of context words (left + right)
@@ -26,9 +26,12 @@ class CBOW(nn.Module):
         # input shape is (B, W)
         e = self.emb(input)
         # e shape is (B, W, E)
-        # u = e.sum(dim=1) # TODO: dot product through dim=1 to apply weigths.
+
+        # u = e.sum(dim=1)
+        # DONE: dot product through dim=1 to apply weigths:
         u = (e * self.weigths.unsqueeze(dim=0).unsqueeze(dim=2)).sum(dim=1)
         # u shape is (B, E)
+
         z = self.lin(u)
         # z shape is (B, V)
         return z
